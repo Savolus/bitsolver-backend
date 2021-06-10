3c460022aaf035fd182e6982df7ffa302c004507
Mykola Dorohyi <37899469+Savolus@users.noreply.github.com>
2021-06-10 19:34:32 +0300

    Update port

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -4,7 +4,7 @@ import { config } from 'dotenv'
 config()
 
 // App
-export const SERVER_PORT = process.env.SERVER_PORT ?? 3000
+export const SERVER_PORT = process.env.PORT ?? 3000
 
 // JWT
 export const JWT_SECRET = process.env.JWT_SECRET
